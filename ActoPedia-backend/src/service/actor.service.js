dcef24f410269e9953c6c2d1001941111213e9b2
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-09 17:04:26 -0600

    update: New image storage format in cloudinary

--- ActoPedia-backend/src/service/actor.service.js
+++ ActoPedia-backend/src/service/actor.service.js
@@ -10,16 +10,32 @@ export const saveActor = async (actor, id) => {
     return newAcot
 }
 
-export const saveImageActor = async (image) => {
-    try {
+export const saveImageActor = async (file) => {
 
-        const result = await cloudinary.uploader.upload(image, {
-            folder: 'strive',
-        })
-        return result.secure_url;
-
-    } catch (err) {
-        console.log("cae en el catch");
-        throw new HttpError(400, err.message);
+    const extension = file.mimetype.split('/')[1];
+    const validExtensions = ['png', 'jpg', 'jpeg'];
+    if (!validExtensions.includes(extension)) {
+        throw new HttpError(400, "Invalid file extension");
     }
-}
+
+    try {
+        return new Promise((resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                {
+                    folder: 'actors',
+                    public_id: file.originalname,
+                },
+                (error, result) => {
+                    if (error) {
+                        return reject(new HttpError(400, error.message));
+                    }
+                    resolve(result.secure_url);
+                }
+            );
+
+            uploadStream.end(file.buffer);
+        });
+    } catch (err) {
+        throw new HttpError(400, `Image upload failed: ${err.message}`);
+    }
+};
