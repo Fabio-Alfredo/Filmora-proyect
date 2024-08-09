7b2ac6b867fb29d00d22f8227cecb634019ade51
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-09 16:28:01 -0600

    update: Multer configuration for handling files and forms

--- ActoPedia-backend/src/app.js
+++ ActoPedia-backend/src/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(multer().none());
+app.use(multer().any());
 app.use(express.json());
 app.use(router);
 dbConnection();
