70f62b0989a707d301395b864b75a9782f6ea617
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-08 22:45:59 -0600

    add: jwt generation

---
+++ ActoPedia-backend/src/utils/jwt.handle.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+const JWT = process.env.JWT_SECRET;
+
+export const sign=(payload)=>{
+    const token = jwt.sign(payload, JWT, {
+        expiresIn: '1h'
+    })
+    return token;
+}
