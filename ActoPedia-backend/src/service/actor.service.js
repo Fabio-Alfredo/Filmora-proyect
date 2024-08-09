80f026d5b3f3f558c0de67e7dd548f7d332f33b4
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-09 10:18:24 -0600

    update: Restructuring of the actor creation service

--- ActoPedia-backend/src/service/actor.service.js
+++ ActoPedia-backend/src/service/actor.service.js
@@ -2,10 +2,7 @@ import { Actor } from '../models/actor.model.js';
 import {verify} from  '../utils/jwt.handle.js';
 import {HttpError} from '../utils/error.handle.js';
 
-export const saveActor = async (actor, token)=>{
-    if(!token) throw new HttpError("Unauthorized")
-    
-    const {id} = verify(token)
-    const newActor = await Actor.create({...actor, createdBy:id})
-    return newActor;
+export const saveActor = async (actor, id)=>{
+    const newAcot = await Actor.create({...actor, createBy:id});
+    return newAcot
 }
