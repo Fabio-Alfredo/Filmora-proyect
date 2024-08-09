e7dee1877882d9b945eeb3f338816562781b04a1
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-09 11:39:31 -0600

    add: image upload service

--- ActoPedia-backend/src/service/actor.service.js
+++ ActoPedia-backend/src/service/actor.service.js
@@ -1,10 +1,24 @@
+import { cloudinary } from '../config/cloudinaryConecction.js';
 import { Actor } from '../models/actor.model.js';
 import { HttpError } from '../utils/error.handle.js';
 
-export const saveActor = async (actor, id)=>{
-    const existActor = await Actor.findOne({name: actor.name});
-    if(existActor)  throw new HttpError(400, "Actor already exist");
-    
-    const newAcot = await Actor.create({...actor, createBy:id});
+export const saveActor = async (actor, id) => {
+    const existActor = await Actor.findOne({ name: actor.name });
+    if (existActor) throw new HttpError(400, "Actor already exist");
+
+    const newAcot = await Actor.create({ ...actor, createBy: id });
     return newAcot
+}
+
+export const saveImageActor = async (image) => {
+    try {
+
+        const result = await cloudinary.uploader.upload(image, {
+            folder: 'strive',
+        })
+        return result.secure_url;
+        
+    } catch (err) {
+        throw new HttpError(400, err.message);
+    }
 }
