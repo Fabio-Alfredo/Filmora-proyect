76115bae346f73f202e440e0c3d9616572c5977d
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-08 18:26:42 -0600

    add: encriptation password

--- ActoPedia-backend/src/service/auth.service.js
+++ ActoPedia-backend/src/service/auth.service.js
@@ -1,10 +1,13 @@
 import { User } from "../models/user.model";
+import {hashPassword, comparePassword} from "../utils/password.handle.js"
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
     const checkUser = await User.findOne({ email:email})
 
     if(checkUser)return console.log("Email already exists")
-    const newUser = await User.create({ name, email, password });
+
+    const pass = await hashPassword(password);
+    const newUser = await User.create({ name, email, password:pass });
     return "user created"   
 }
