58ec3442cd80a88de6502b75d734061a37766180
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-08 18:17:07 -0600

    add: password verification

--- ActoPedia-backend/src/utils/password.handle.js
+++ ActoPedia-backend/src/utils/password.handle.js
@@ -4,3 +4,8 @@ export const hashPassword = async (password) => {
     const hashPassword = await bcrypt.hash(password, 10);
     return hashPassword;
 }
+
+export const comparePassword = async (password, hashPassword) => {
+    const isCorrect = await bcrypt.compare(password, hashPassword);
+    return isCorrect;
+}
