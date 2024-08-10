62149b6e1687a9d3d592dc7319dcc09facd55caa
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-09 22:50:42 -0600

    add: route get actors and get actors by name

--- ActoPedia-backend/src/routes/actor.route.js
+++ ActoPedia-backend/src/routes/actor.route.js
@@ -1,10 +1,18 @@
-import { saveActorController } from "../controllers/actor.controller.js";
+import {
+    saveActorController,
+    getActorsController,
+    getActorsByNameController,
+    deleteActorController
+} from "../controllers/actor.controller.js";
 import { Router } from 'express';
 
 const router = Router();
 
 router.post("/create", saveActorController);
+router.get("/authors", getActorsController);
+router.get("/getByName", getActorsByNameController);
+router.delete("/delete/:id", deleteActorController);
 
-export{
+export {
     router
 }
