20c7a7b6200298db4d56a6b269ca3f7123fdbc82
Fabio-Alfredo <00210121@uca.edu.sv>
2024-08-10 23:35:30 -0600

    update: new campo form

--- ActoPedia-frontend/ActoPedia/src/components/FormActorsComponents/MediaForm.jsx
+++ ActoPedia-frontend/ActoPedia/src/components/FormActorsComponents/MediaForm.jsx
@@ -7,12 +7,12 @@ import { IoIosArrowDown } from "react-icons/io";
 const MediaForm = ({ input, tittle, data }) => {
 
     const [movies, setMovies] = useState([]);
-    const { title, personage, year, InputChange, resetForm } = useForm({ title: '', personage: '', year: '' });
+    const { title, personage, description, InputChange, resetForm } = useForm({ title: '', personage: '', description: '' });
     const [expanded, setExpanded] = useState(false);
 
     const addMovie = () => {
-        if (title && personage && year) {
-            const formValues = { title, personage, year };
+        if (title && personage && description) {
+            const formValues = { title, personage, description };
             const newItem = [...movies, formValues]
             setMovies(newItem);
             resetForm();
@@ -36,7 +36,7 @@ const MediaForm = ({ input, tittle, data }) => {
                     <h3 className='font-bold'>Película {index + 1}</h3>
                     <p className='font-bold'>Título: {movie.title}</p>
                     <p className='font-bold'>Personaje: {movie.personage}</p>
-                    <p className='font-bold'>{input} {movie.year}</p>
+                    <p className='font-bold'>{input} {movie.description}</p>
                 </div>
             ))}
             {
@@ -44,7 +44,7 @@ const MediaForm = ({ input, tittle, data }) => {
                     <div className="flex flex-col gap-5 w-4/5 3xl:w-3/4 items-center">
                         <InputField nameField='Titulo' inputName='title' type='text' inputValue={title} inputOnchage={InputChange} />
                         <InputField nameField='Personaje' inputName='personage' type='text' inputValue={personage} inputOnchage={InputChange} />
-                        <InputField nameField='Year' inputName='year' type='number' inputValue={year} inputOnchage={InputChange} />
+                        <InputField nameField='description' inputName='description' type='number' inputValue={description} inputOnchage={InputChange} />
                         <input onClick={addMovie} className="bg-black min-w-[50%] p-4 text-white font-Roboto self-end rounded-xl mt-6 hover:bg-slate-100/70 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-black" type="submit" value="Agregar" />
 
                     </div>
